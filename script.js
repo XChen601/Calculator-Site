26bcfecca03d380b1af86f7a98b1c35ca4a65454
xchen <xchen601@gmail.com>
2023-01-14 15:04:06 -0500

    css and html near completion, added colors, button style, and number and operator functions

---
+++ script.js
@@ -0,0 +1,91 @@
+
+
+
+
+// the maths
+
+let prevNum = ""
+let currentNum = ""
+let symbol = ""
+
+// number buttons
+const numbers = document.querySelectorAll(".numbers")
+numbers.forEach(number => number.addEventListener('click', (e) => {
+    currentNum += number.textContent
+    updateMainDisplay(currentNum)
+}))
+
+const operationBtns = document.querySelectorAll(".operations")
+operationBtns.forEach(operationBtn => operationBtn.addEventListener('click', (e) => { 
+    // if there are 2 values
+    if (prevNum !== "" && currentNum !== ""){
+        console.log(`${prevNum} ${symbol} ${currentNum}`)
+        let answer = calculate();
+        console.log(answer);
+        updateMainDisplay(answer);
+        prevNum = answer;
+    }
+    // do nothing if value at prev num, next value will be added and it will calculate
+    else if (prevNum !== "") {
+
+    }
+    // case if prev num has nothing while current num has value
+    else {
+        prevNum = currentNum;
+    }
+    symbol = operationBtn.textContent;
+    updateUpperDisplay(symbol)
+    currentNum = "";  
+}))
+
+function updateUpperDisplay(string) {
+    if (string === '=') {
+        string = '';
+    }
+    const upperDisplay = document.querySelector("#upperDisplay");
+    upperDisplay.textContent = string;
+}
+
+function updateMainDisplay(string) {
+    const display = document.querySelector("#mainDisplay");
+    display.textContent = string;
+}
+function calculate() {
+    if(symbol === '+'){
+        result = add(prevNum,currentNum);
+    }
+    else if (symbol === '='){
+        result = equal(prevNum, currentNum)
+    }
+    else if (symbol === '-'){
+        result = subtract(prevNum, currentNum)
+    }
+    else if(symbol === '*'){
+        result = multiply(prevNum, currentNum)
+    }
+    else if (symbol === '/'){
+        result = divide(prevNum, currentNum)
+    }
+    return Math.round(result * 100000) / 100000;
+}
+
+
+function add(a, b) {
+    return parseFloat(a) + parseFloat(b);
+}
+
+function subtract(a, b) {
+    return parseFloat(a) - parseFloat(b);
+}
+
+function multiply(a,b) {
+    return parseFloat(a) * parseFloat(b);
+}
+
+function divide(a,b) {
+    return parseFloat(a) / parseFloat(b);
+}
+
+function equal(a,b) {
+    return b
+}
